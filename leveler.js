fc36c6ad387522e62c71a102a6fb3a4dd599a3cc
Parker <Kaninepete@gmail.com>
2013-03-31 21:08:16 -0400

    figured out spacing

--- leveler.js
+++ leveler.js
@@ -1,4 +1,5 @@
 function Grid(w,h){
+	this.pixels = 32;
     this.grid=[];
     var row;
     for(i=0;i<h;i++){
@@ -10,22 +11,28 @@ function Grid(w,h){
 	}
 	this.save = function(x,y,value){//use point as argument?
 		c = this.grid[y-1];
-        c[x-1]=value;
-	};
+		c[x-1]=value;
+	}
 	this.read = function(x,y){//use point as argument?
 		c = this.grid[y-1];
-        return c[x-1];
-	};
+		return c[x-1];
+	}
 	this.draw = function(){
-	for(i=0;i<h;i++){
-		for(var n=0;n<w;n++){
-			//draw square(h,n)
+		for(i=0;i<h;i++){
+			for(var n=0;n<w;n++){
+			//this.read(h,n);
+			}
 		}
 	}
 }
- var test= new Grid(4,4);
+var test= new Grid(4,4);
  
- var key = {
+function Block(){
+	
+ }
+ 
+ /*var key = {
 	 0:empty,
 	 1:block
 	 };
+	 */
