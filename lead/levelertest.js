77b302f3b7168310e857f232cb436aaad9aa6b08
Parker <Kaninepete@gmail.com>
2013-07-02 18:33:42 -0400

    starting on image loader

--- lead/levelertest.js
+++ lead/levelertest.js
@@ -6,16 +6,21 @@ display.ctx.fillRect(10, 10, 10, 10);
 //var test = new Grid(1, 1);
 //test.draw(display.ctx);
 //var imgX = document.getElementById("test");
-var imgO = loadPic("testImage.png");
-var imgS = new Sequence();
-imgS.load(["testImage.png"]);
+//var imgO = loadPic("testImage.png");
+//var imgS = new Sequence();
+//imgS.load(["testImage.png"]);
 //console.log(imgX);
-console.log(imgO);
+//console.log(imgO);
 //display.ctx.drawImage(imgX,10,10);
-display.ctx.drawImage(imgO,30,30);
-display.ctx.drawImage(imgS.get(),50,50);
+function drawcoins(){
+	display.ctx.drawImage(imgO,30,30);
+	//display.ctx.drawImage(imgS.get(),50,50);
+}
 
 /*==============
-loadPic() has no check for if the image is loaded or not.
-Trying to draw the image too soon happens often.
+*loadPic() has no check for if the image is loaded or not.
+*Trying to draw the image too soon happens often.
+* 
+* Try this:
+* http://gamedev.stackexchange.com/questions/24102/resource-loader-for-an-html5-and-javascript-game
 */
---
+++ lead/levelertest_preload.js
@@ -0,0 +1,8 @@
+var imgO = loadPic("testImage.png");
+//var imgS = new Sequence();
+//imgS.load(["testImage.png"]);
+//console.log(imgX);
+console.log(imgO);
+//display.ctx.drawImage(imgX,10,10);
+drawcoins()
+
