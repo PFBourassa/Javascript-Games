add588f3b6a1bb0ddb228c76a84f8d0f4267c1f7
Parker <Kaninepete@gmail.com>
2013-07-02 18:10:14 -0400

    whitespace cleanup

--- lead/levelertest.js
+++ lead/levelertest.js
@@ -1,9 +1,21 @@
+if (true){
+	console.log("display.ctx: " + display.canvas);
+}
+
+display.ctx.fillRect(10, 10, 10, 10);
 //var test = new Grid(1, 1);
-	//test.draw(display.ctx);
-	var imgX = document.getElementById("test");//loadPic("testImage.png");
-	var imgO = loadPic("testImage.png");
-	console.log(imgX);
-	console.log(imgO);
-	display.ctx.fillRect(10, 10, 10, 10);
-	//display.ctx.drawImage(imgX,10,10);
-	display.ctx.drawImage(imgO,30,30);
+//test.draw(display.ctx);
+//var imgX = document.getElementById("test");
+var imgO = loadPic("testImage.png");
+var imgS = new Sequence();
+imgS.load(["testImage.png"]);
+//console.log(imgX);
+console.log(imgO);
+//display.ctx.drawImage(imgX,10,10);
+display.ctx.drawImage(imgO,30,30);
+display.ctx.drawImage(imgS.get(),50,50);
+
+/*==============
+loadPic() has no check for if the image is loaded or not.
+Trying to draw the image too soon happens often.
+*/
