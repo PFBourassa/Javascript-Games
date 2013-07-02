64ca06cf6fff049e82a82449618fcf095a65ffa3
Parker <Kaninepete@gmail.com>
2013-07-02 19:32:38 -0400

    changed test.png to be more visable

--- lead/levelertest_preload.js
+++ lead/levelertest_preload.js
@@ -1,9 +1,8 @@
 var imgO = loadPic("testImage.png");
-//var imgS = new Sequence();
-//imgS.load(["testImage.png"]);
-//console.log(imgX);
+
 console.log(imgO);
-//display.ctx.drawImage(imgX,10,10);
+
 imgO.onload = function() {
   drawcoins()
 }
+
