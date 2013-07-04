700359d80fa95de918c26e9fdfdef69970654e71
Parker <Kaninepete@gmail.com>
2013-07-04 11:36:17 -0400

    add elysium.js, and brennans secret message

---
+++ elysium/elysium.js
@@ -0,0 +1,7 @@
+/*=======================================================
+Brennan, this is the secret message I said I would add:
+
+"Bill Clinton"
+
+This is to make sure that you have the newest version.
+=======================================================*/
